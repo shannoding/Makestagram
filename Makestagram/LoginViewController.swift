bfa3c63c9a78f6a8fb17032d20521dfb8b80ea94
Shannon Ding <shannon_ding@yahoo.com>
2017-06-26 13:26:58 -0700

    Initial commit

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,75 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Shannon Ding on 6/25/17.
+//  Copyright © 2017 Shannon Ding. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+   
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+
+            // 1
+    guard let authUI = FUIAuth.defaultAuthUI()
+                else { return }
+            
+            // 2
+            authUI.delegate = self
+            
+            // 3
+            let authViewController = authUI.authViewController()
+            present(authViewController, animated: true)
+        }
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        
+        
+        UserService.show(forUID: user.uid) { (user) in
+            if let user = user {
+                // handle existing user
+                User.setCurrent(user)
+                
+                let storyboard = UIStoryboard(name: "Main", bundle: .main)
+                if let initialViewController = storyboard.instantiateInitialViewController() {
+                    self.view.window?.rootViewController = initialViewController
+                    self.view.window?.makeKeyAndVisible()
+                }
+            } else {
+                // handle new user
+                self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+            }
+        }
+    }
+}
+
---
+++ Makestagram/User.swift
@@ -0,0 +1,53 @@
+//
+//  User.swift
+//  Makestagram
+//
+//  Created by Shannon Ding on 6/25/17.
+//  Copyright © 2017 Shannon Ding. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase.FIRDataSnapshot
+
+class User {
+    
+    // MARK: - Properties
+    
+    let uid: String
+    let username: String
+    
+    // MARK: - Init
+    
+    init(uid: String, username: String) {
+        self.uid = uid
+        self.username = username
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard let dict = snapshot.value as? [String : Any],
+            let username = dict["username"] as? String
+            else { return nil }
+        
+        self.uid = snapshot.key
+        self.username = username
+    }
+    // MARK: - Singleton
+    private static var _current: User?
+
+    static var current: User {
+        // 3
+        guard let currentUser = _current else {
+            fatalError("Error: current user doesn't exist")
+        }
+        
+        // 4
+        return currentUser
+    }
+    
+    // MARK: - Class Methods
+    
+    // 5
+    static func setCurrent(_ user: User) {
+        _current = user
+    }
+}
