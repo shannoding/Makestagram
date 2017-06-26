21e36f1de2e7a40c9900e8ac57eeb7385566dd79
Shannon Ding <shannon_ding@yahoo.com>
2017-06-26 14:00:37 -0700

    everything again

---
+++ Makestagram/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Makestagram
+//
+//  Created by Shannon Ding on 6/26/17.
+//  Copyright © 2017 Shannon Ding. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
