b3f2ae436f90861706cd246009304d6726f1da67
Shannon Ding <shannon_ding@yahoo.com>
2017-06-27 15:53:44 -0700

    improving timeline and making a home page

--- Makestagram/HomeViewController.swift
+++ Makestagram/HomeViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2017 Shannon Ding. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [Post]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+    }
+    
+}
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let post = posts[indexPath.row]
+        
+        return post.imageHeight
+    }
+}
